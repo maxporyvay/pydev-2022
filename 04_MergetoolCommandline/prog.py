b290f598796bf72330c89102df3849bc631cacbf
maxporyvay <sevmaxclever@gmail.com>
2022-03-08 01:53:45 +0300

    Добавлена функция language
    В программу добавлена функция language, меняющая язык для всех генераторов. Также немного поправлена функция generate

--- 04_MergetoolCommandline/prog.py
+++ 04_MergetoolCommandline/prog.py
@@ -7,8 +7,6 @@ from pynames.generators.iron_kingdoms import *
 from pynames import GENDER, LANGUAGE
 
 
-current_language = LANGUAGE.NATIVE
-
 simple_generators = {'scandinavian': scandinavian.ScandinavianNamesGenerator,
                      'russian': russian.PaganNamesGenerator,
                      'mongolian': mongolian.MongolianNamesGenerator,
@@ -34,6 +32,24 @@ class Repl(cmd.Cmd):
     
     prompt = '>'
 
+    current_language = LANGUAGE.NATIVE
+
+    def do_language(self, arg):
+        args = shlex.split(arg, comments=True)
+        if len(args) < 1:
+            return
+        if len(args) == 1:
+            if args[0] in ['ru', 'RU']:
+                self.current_language = LANGUAGE.RU
+            elif args[0] in ['en', 'EN']:
+                self.current_language = LANGUAGE.EN
+            elif args[0] in ['native', 'NATIVE']:
+                self.current_language = LANGUAGE.NATIVE
+            else:
+                return
+        else:
+            return
+
     def do_generate(self, arg):
         args = shlex.split(arg, comments=True)
         if len(args) < 1:
@@ -41,61 +57,61 @@ class Repl(cmd.Cmd):
         if args[0] in simple_generators:
             gen = simple_generators[args[0]]
             if len(args) == 1:
-                print(gen().get_name_simple(GENDER.MALE, current_language))
+                print(gen().get_name_simple(GENDER.MALE, self.current_language))
             elif len(args) == 2:
                 if args[1] == 'male':
-                    print(gen().get_name_simple(GENDER.MALE, current_language))
+                    print(gen().get_name_simple(GENDER.MALE, self.current_language))
                 elif args[1] == 'female':
-                    print(gen().get_name_simple(GENDER.FEMALE, current_language))
+                    print(gen().get_name_simple(GENDER.FEMALE, self.current_language))
                 else:
                     return
             else: return
         elif args[0] == 'elven':
             if len(args) == 1:
-                print(elven_generators['warhammer']().get_name_simple(GENDER.MALE, current_language))
+                print(elven_generators['warhammer']().get_name_simple(GENDER.MALE, self.current_language))
             elif len(args) == 2:
                 if args[1] in ['male', 'female']:
                     if args[1] == 'male':
-                        print(elven_generators['warhammer']().get_name_simple(GENDER.MALE, current_language))
+                        print(elven_generators['warhammer']().get_name_simple(GENDER.MALE, self.current_language))
                     elif args[1] == 'female':
-                        print(elven_generators['warhammer']().get_name_simple(GENDER.FEMALE, current_language))
+                        print(elven_generators['warhammer']().get_name_simple(GENDER.FEMALE, self.current_language))
                 elif args[1] in elven_generators:
                     gen = elven_generators[args[1]]
-                    print(gen().get_name_simple(GENDER.MALE, current_language))
+                    print(gen().get_name_simple(GENDER.MALE, self.current_language))
                 else:
                     return
             elif len(args) == 3:
                 if args[1] in elven_generators and args[2] in ['male', 'female']:
                     gen = elven_generators[args[1]]
                     if args[2] == 'male':
-                        print(gen().get_name_simple(GENDER.MALE, current_language))
+                        print(gen().get_name_simple(GENDER.MALE, self.current_language))
                     elif args[2] == 'female':
-                        print(gen().get_name_simple(GENDER.FEMALE, current_language))
+                        print(gen().get_name_simple(GENDER.FEMALE, self.current_language))
                 else:
                     return
             else:
                 return
         elif args[0] == 'iron_kingdoms':
             if len(args) == 1:
-                print(iron_kingdoms_generators['caspian_midlunder_sulese']().get_name_simple(GENDER.MALE, current_language))
+                print(iron_kingdoms_generators['caspian_midlunder_sulese']().get_name_simple(GENDER.MALE, self.current_language))
             elif len(args) == 2:
                 if args[1] in ['male', 'female']:
                     if args[1] == 'male':
-                        print(iron_kingdoms_generators['caspian_midlunder_sulese']().get_name_simple(GENDER.MALE, current_language))
+                        print(iron_kingdoms_generators['caspian_midlunder_sulese']().get_name_simple(GENDER.MALE, self.current_language))
                     elif args[1] == 'female':
-                        print(iron_kingdoms_generators['caspian_midlunder_sulese']().get_name_simple(GENDER.FEMALE, current_language))
+                        print(iron_kingdoms_generators['caspian_midlunder_sulese']().get_name_simple(GENDER.FEMALE, self.current_language))
                 elif args[1] in iron_kingdoms_generators:
                     gen = iron_kingdoms_generators[args[1]]
-                    print(gen().get_name_simple(GENDER.MALE, current_language))
+                    print(gen().get_name_simple(GENDER.MALE, self.current_language))
                 else:
                     return
             elif len(args) == 3:
                 if args[1] in iron_kingdoms_generators and args[2] in ['male', 'female']:
-                    gen = rhiron_kingdoms_generators[args[1]]
+                    gen = iron_kingdoms_generators[args[1]]
                     if args[2] == 'male':
-                        print(gen().get_name_simple(GENDER.MALE, current_language))
+                        print(gen().get_name_simple(GENDER.MALE, self.current_language))
                     elif args[2] == 'female':
-                        print(gen().get_name_simple(GENDER.FEMALE, current_language))
+                        print(gen().get_name_simple(GENDER.FEMALE, self.current_language))
                 else:
                     return
             else:
