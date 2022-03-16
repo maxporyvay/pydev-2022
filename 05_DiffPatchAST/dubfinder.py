59e90ca53d6d16d985d04075caf147afcaad5c36
maxporyvay <sevmaxclever@gmail.com>
2022-03-16 14:32:02 +0300

    Для всех необходимых функций в модулях получены деревья разборов с помощью ast

--- 05_DiffPatchAST/dubfinder.py
+++ 05_DiffPatchAST/dubfinder.py
@@ -3,3 +3,26 @@ import ast
 import inspect
 import difflib
 import textwrap
+import sys
+
+funcdict = {}
+
+
+def myInspect(item, prefix):
+    for name, value in inspect.getmembers(item):
+        if inspect.isfunction(value):
+            funcdict[prefix + name] = value
+        elif inspect.isclass(value) and (len(name) == 1 or name[:2] != '__'):
+            myInspect(value, prefix + name + '.')
+
+
+for modidx in range(1, len(sys.argv)):
+    modulename = sys.argv[modidx]
+    module = importlib.import_module(modulename)
+    myInspect(module, modulename + '.')
+
+for k, v in funcdict.items():
+    text = textwrap.dedent(inspect.getsource(v))
+    tree = ast.parse(text)
+    
+    
