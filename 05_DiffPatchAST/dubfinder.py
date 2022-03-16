7c9a50c5a55ddf7b8b58d582e7e0893946a6cd17
maxporyvay <sevmaxclever@gmail.com>
2022-03-16 15:47:17 +0300

    Завершена работа над программой dubfinder

--- 05_DiffPatchAST/dubfinder.py
+++ 05_DiffPatchAST/dubfinder.py
@@ -24,5 +24,33 @@ for modidx in range(1, len(sys.argv)):
 for k, v in funcdict.items():
     text = textwrap.dedent(inspect.getsource(v))
     tree = ast.parse(text)
-    
+    for node in ast.walk(tree):
+        if 'name' in dir(node):
+            node.name = '_'
+        if 'id' in dir(node):
+            node.id = '_'
+        if 'attr' in dir(node):
+            node.attr = '_'
+        if 'arg' in dir(node):
+            node.arg = '_'
+    modifiedtext = ast.unparse(tree)
+    funcdict[k] = modifiedtext
+
+pairs = []
+
+for k, v in funcdict.items():
+    maxratio = -1
+    pos = -1
+    for k1, v1 in funcdict.items():
+        if k != k1:
+            ratio = difflib.SequenceMatcher(None, v, v1).ratio()
+            if ratio > maxratio:
+                maxratio = ratio
+                pos = k1
+    if maxratio > 0.95 and (pos, k) not in pairs:
+        pairs.append((k, pos))
+
+for k1, k2 in sorted(pairs):
+    print(k1, k2, sep=' : ')
+            
     
